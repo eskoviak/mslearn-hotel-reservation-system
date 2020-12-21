561006dc9ce9eecd5bbdc634da1a3a7ede461902
Ed Skoviak <eskoviak@eskoviak.com>
2020-12-21 15:56:00 -0600

    completed unit

---
+++ src/BuildAZ.ps1
@@ -0,0 +1,4 @@
+az container create --resource-group App_tutorial_rg `
+  --name esc-instance --image ESCAppRegistry.azurecr.io/reservationsystem:v1 `
+  --dns-name-label ESCDNSReservationSystem `
+  --registry-username ESCAppRegistry --registry-password B7DNzRK3hictFQtozfjioVI5XHruBv+d 
